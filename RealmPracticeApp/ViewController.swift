63c822e4eea05e49fbde4355772e64d2765ba9af
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2024-08-29 17:31:40 +0300

    Initial Commit

---
+++ RealmPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPracticeApp
+//
+//  Created by Виталий Подшибякин on 29.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
